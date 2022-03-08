f6429f89667de2ce9c8b9b8fb8ece80da7a3806e
xposbda <xpos.bda@gmail.com>
2022-03-08 15:27:04 +0700

    menu1 tes

--- otoritas_smERP/Form1.Designer.vb
+++ otoritas_smERP/Form1.Designer.vb
@@ -22,137 +22,145 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.chx023 = New System.Windows.Forms.CheckBox()
-        Me.chx021 = New System.Windows.Forms.CheckBox()
-        Me.chx022 = New System.Windows.Forms.CheckBox()
-        Me.chx02 = New System.Windows.Forms.CheckBox()
-        Me.chx01 = New System.Windows.Forms.CheckBox()
-        Me.chx0 = New System.Windows.Forms.CheckBox()
+        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Exit")
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Add User")
+        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Edit User")
+        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Login Role", New System.Windows.Forms.TreeNode() {TreeNode2, TreeNode3})
+        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ganti Pass")
+        Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Add Role")
+        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Edit Role")
+        Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("User Role", New System.Windows.Forms.TreeNode() {TreeNode6, TreeNode7})
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.TabPage6 = New System.Windows.Forms.TabPage()
+        Me.TabPage5 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.TabControl1.SuspendLayout()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TabControl1
+        'TreeView1
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(134, 210)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(1004, 244)
-        Me.TabControl1.TabIndex = 13
+        Me.TreeView1.CheckBoxes = True
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode1.Name = "Node0"
+        TreeNode1.Text = "Exit"
+        TreeNode2.Name = "Node4"
+        TreeNode2.Text = "Add User"
+        TreeNode3.Name = "Node5"
+        TreeNode3.Text = "Edit User"
+        TreeNode4.Name = "Node1"
+        TreeNode4.Text = "Login Role"
+        TreeNode5.Name = "Node2"
+        TreeNode5.Text = "Ganti Pass"
+        TreeNode6.Name = "Node6"
+        TreeNode6.Text = "Add Role"
+        TreeNode7.Name = "Node7"
+        TreeNode7.Text = "Edit Role"
+        TreeNode8.Name = "Node3"
+        TreeNode8.Text = "User Role"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode1, TreeNode4, TreeNode5, TreeNode8})
+        Me.TreeView1.Size = New System.Drawing.Size(609, 328)
+        Me.TreeView1.TabIndex = 6
         '
-        'TabPage1
+        'TabPage6
         '
-        Me.TabPage1.Controls.Add(Me.chx023)
-        Me.TabPage1.Controls.Add(Me.chx021)
-        Me.TabPage1.Controls.Add(Me.chx022)
-        Me.TabPage1.Controls.Add(Me.chx02)
-        Me.TabPage1.Controls.Add(Me.chx01)
-        Me.TabPage1.Controls.Add(Me.chx0)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(996, 218)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.TabPage6.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage6.Name = "TabPage6"
+        Me.TabPage6.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage6.Size = New System.Drawing.Size(609, 328)
+        Me.TabPage6.TabIndex = 5
+        Me.TabPage6.Text = "Akunting"
+        Me.TabPage6.UseVisualStyleBackColor = True
         '
-        'chx023
+        'TabPage5
         '
-        Me.chx023.AutoSize = True
-        Me.chx023.Location = New System.Drawing.Point(70, 137)
-        Me.chx023.Name = "chx023"
-        Me.chx023.Size = New System.Drawing.Size(153, 17)
-        Me.chx023.TabIndex = 5
-        Me.chx023.Text = "Lokasi Barang Per Cabang"
-        Me.chx023.UseVisualStyleBackColor = True
+        Me.TabPage5.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage5.Name = "TabPage5"
+        Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage5.Size = New System.Drawing.Size(609, 328)
+        Me.TabPage5.TabIndex = 4
+        Me.TabPage5.Text = "Warehouse"
+        Me.TabPage5.UseVisualStyleBackColor = True
         '
-        'chx021
+        'TabPage4
         '
-        Me.chx021.AutoSize = True
-        Me.chx021.Location = New System.Drawing.Point(70, 91)
-        Me.chx021.Name = "chx021"
-        Me.chx021.Size = New System.Drawing.Size(93, 17)
-        Me.chx021.TabIndex = 4
-        Me.chx021.Text = "Setting Lokasi"
-        Me.chx021.UseVisualStyleBackColor = True
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(609, 328)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Data Penjualan"
+        Me.TabPage4.UseVisualStyleBackColor = True
         '
-        'chx022
+        'TabPage3
         '
-        Me.chx022.AutoSize = True
-        Me.chx022.Location = New System.Drawing.Point(70, 114)
-        Me.chx022.Name = "chx022"
-        Me.chx022.Size = New System.Drawing.Size(84, 17)
-        Me.chx022.TabIndex = 3
-        Me.chx022.Text = "Input Lokasi"
-        Me.chx022.UseVisualStyleBackColor = True
-        '
-        'chx02
-        '
-        Me.chx02.AutoSize = True
-        Me.chx02.Location = New System.Drawing.Point(51, 68)
-        Me.chx02.Name = "chx02"
-        Me.chx02.Size = New System.Drawing.Size(94, 17)
-        Me.chx02.TabIndex = 2
-        Me.chx02.Text = "Lokasi Barang"
-        Me.chx02.UseVisualStyleBackColor = True
-        '
-        'chx01
-        '
-        Me.chx01.AutoSize = True
-        Me.chx01.Location = New System.Drawing.Point(72, 45)
-        Me.chx01.Name = "chx01"
-        Me.chx01.Size = New System.Drawing.Size(92, 17)
-        Me.chx01.TabIndex = 1
-        Me.chx01.Text = "Daftar Barang"
-        Me.chx01.UseVisualStyleBackColor = True
-        '
-        'chx0
-        '
-        Me.chx0.AutoSize = True
-        Me.chx0.Location = New System.Drawing.Point(51, 22)
-        Me.chx0.Name = "chx0"
-        Me.chx0.Size = New System.Drawing.Size(113, 17)
-        Me.chx0.TabIndex = 0
-        Me.chx0.Text = "Data Bantu Umum"
-        Me.chx0.UseVisualStyleBackColor = True
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(609, 328)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Data Pembelian"
+        Me.TabPage3.UseVisualStyleBackColor = True
         '
         'TabPage2
         '
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(996, 218)
+        Me.TabPage2.Size = New System.Drawing.Size(609, 328)
         Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.Text = "Data Bantu Umum"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.TreeView1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(609, 328)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "FIle"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Controls.Add(Me.TabPage5)
+        Me.TabControl1.Controls.Add(Me.TabPage6)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 9)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(617, 354)
+        Me.TabControl1.TabIndex = 13
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1272, 664)
+        Me.ClientSize = New System.Drawing.Size(636, 377)
         Me.Controls.Add(Me.TabControl1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
-        Me.TabPage1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents chx023 As CheckBox
-    Friend WithEvents chx021 As CheckBox
-    Friend WithEvents chx022 As CheckBox
-    Friend WithEvents chx02 As CheckBox
-    Friend WithEvents chx01 As CheckBox
-    Friend WithEvents chx0 As CheckBox
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents TabPage6 As TabPage
+    Friend WithEvents TabPage5 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents TabPage3 As TabPage
     Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabControl1 As TabControl
 End Class
